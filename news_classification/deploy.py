14bff8135db2c92a48dd73b6f258a8093c2b3790
kritagya Ghimire <97329413+kritagya97@users.noreply.github.com>
2025-03-04 23:30:36 +0545

    Update deploy.py

--- news_classification/deploy.py
+++ news_classification/deploy.py
@@ -1,73 +1,3 @@
-# import streamlit as st
-# import nltk
-# nltk.download('stopwords')
-# nltk.download('punkt') # If tokenization is used
-# nltk.download('wordnet')  # If lemmatization is used
-# nltk.download('omw-1.4')
-# import joblib
-# import re 
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-# from nltk.stem import WordNetLemmatizer
-
-# lemmatizer = WordNetLemmatizer()
-# stop_words = set(stopwords.words('english'))
-
-# # Load pre-trained model
-# model = joblib.load('news_classification/nb_model.joblib')
-
-# # Streamlit app layout
-# st.set_page_config(page_title="News Categorization", page_icon="📚", layout="centered")
-
-# # App header
-# st.title("News Categorization App")
-# st.markdown("""
-#     ### Enter a news headline or text to categorize it
-#     This app uses a machine learning model to predict the category of the given news text.
-#     """)
-
-# # Add some space
-# st.markdown("<hr>", unsafe_allow_html=True)
-
-# # User input text
-# input_text = st.text_area("Enter News Text", height=200)
-
-# def preprocess_text(text):
-#     # Lowercasing
-#     text = text.lower()
-#     # Removing special characters and punctuation
-#     text = re.sub(r'[^a-zA-Z\s]', '', text)
-#     # Tokenization
-#     words = word_tokenize(text)
-#     # Remove stopwords and lemmatize
-#     words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
-#     # Join back into a string
-#     return ' '.join(words)
-
-# # Function to predict category
-# def predict_category(text):
-#     # Preprocess the text
-#     processed_text = preprocess_text(text)
-#     # Make the prediction using the model
-#     prediction = model.predict([processed_text])
-#     return prediction[0]
-
-# # Display prediction result when button is pressed
-# if st.button("Predict Category"):
-#     if input_text:
-#         category = predict_category(input_text)
-#         st.markdown(f"**Predicted Category:** {category}")
-#     else:
-#         st.warning("Please enter a news text to categorize.")
-
-# # Adding a footer
-# st.markdown("<hr>", unsafe_allow_html=True)
-# st.markdown("""
-#     Built by Kritagya_Ghimire!
-#     """)
-
-
-
 import streamlit as st
 import nltk
 import os
@@ -84,6 +14,7 @@ nltk.data.path.append(nltk_data_path)
 # Ensure necessary NLTK datasets are downloaded
 nltk.download('stopwords', download_dir=nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)  # Add this line
 nltk.download('wordnet', download_dir=nltk_data_path)
 nltk.download('omw-1.4', download_dir=nltk_data_path)
 
